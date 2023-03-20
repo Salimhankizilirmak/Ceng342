5dddbf813e4d027f2dcfca7d8525cd2d27f9efdc
Salimhankizilirmak <127133041+Salimhankizilirmak@users.noreply.github.com>
2023-03-20 23:03:33 +0300

    Add files via upload

--- First Homework/19050111055.c
+++ First Homework/19050111055.c
@@ -6,54 +6,50 @@
 int main(int argc, char *argv[]) {
  
 
-    int n = atoi(argv[1]);
-    int m = atoi(argv[2]);
+    int rows = atoi(argv[1]);
+    int cols = atoi(argv[2]);
     char *output_file = argv[3];
 
-    double **matrix = (double **) malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (double *) calloc(m, sizeof(double));
+    double **matrix = (double **) malloc(rows * sizeof(double *));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (double *) calloc(cols, sizeof(double));
     }
+    double *result = (double *) malloc(rows * sizeof(double));
+    double *vector = (double *) malloc(cols * sizeof(double));
 
-    double *vector = (double *) malloc(m * sizeof(double));
-    double *result = (double *) malloc(n * sizeof(double));
-
-
-    // Set random seed based on student ID
     
     srand((unsigned short)19050111055);
 
-
-    // Fill matrix with random double values between [1.0 - 100.0]
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             matrix[i][j] = ((double) rand() / RAND_MAX) * 99.0 + 1.0;
         }
     }
 
     // Create random vector
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < cols; i++) {
         vector[i] = ((double) rand() / RAND_MAX) * 99.0 + 1.0;
     }
 
-    // Perform matrix-vector multiplication
-    multiplication(matrix, vector, n, m, result);
+ 
+    multiplication(matrix, vector, rows, cols, result);
 
-    // Write solution to file
+
     FILE *fp;
     fp = fopen(output_file, "w");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < rows; i++) {
         fprintf(fp, "%.2f\n", result[i]);
     }
     fclose(fp);
 
-    // Free allocated memory
-    for (int i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
-   if (argc < 4) {
+
+    
+   if (argc != 4) {
         printf("Usage: ./matrix-vector <n> <m> <output_file>\n");
         return 1;
+    }
+	for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
     }
     free(matrix);
     free(vector);
